cf0737f01625f5fe776a74bfd2286af291148a4a
jihalee <jihalee@student.42.fr>
2023-12-08 02:48:25 +0100

    ex04 done

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 
 int main(int argc, char **argv) {
-	if (argc != 4)
+
+	if (argc != 4) {
 		std::cerr << "invalid number of arguments" << std::endl;
+		return (1);
+	}
+	std::string filename(argv[1]);
+	std::ifstream ifs(&filename[0]);
+	std::string content((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));
+	ifs.close();
+	std::string s1(argv[2]);
+	std::string s2(argv[3]);
+	size_t pos;
+
+	pos = 0;
+	while (true) {
+		pos = content.find(s1, pos);
+		if (pos == std::string::npos)
+			break;
+		content.erase(pos, s1.length());
+		content.insert(pos, s2);
+	}
+
+	filename.append(".replace");
+	std::ofstream ofs(&filename[0]);
+	ofs << content << std::endl;
+	ofs.close();
+	return 0;
 }
